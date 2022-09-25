1ea64518ddf5a0df2be4031dd5796c081bfa74bb
dcDalin <mcdalinoluoch@gmail.com>
2022-09-25 19:04:16 +0300

    feat: getArnieQuotes

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,33 @@
-import { httpGet } from './mock-http-interface';
+import { httpGet } from "./mock-http-interface";
 
-// TODO define this type properly
-type TResult = any;
+enum ResponseEnum {
+  "Arnie Quote",
+  FAILURE,
+}
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+type TResult = { [key in keyof typeof ResponseEnum]?: string };
+
+const requestQuoteHttp = async (url: string): Promise<TResult> => {
+  const { status, body } = await httpGet(url);
+
+  const { message }: { message: string } = JSON.parse(body);
+
+  if (status === 200) {
+    return { "Arnie Quote": message };
+  } else {
+    return { FAILURE: message };
+  }
+};
+
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
+  // check if urls are passed when calling the function
+  if (urls.length === 0) {
+    return [];
+  } else {
+    const arnieQuotes = await Promise.all(
+      urls.map(async (url) => await requestQuoteHttp(url))
+    );
+
+    return arnieQuotes;
+  }
 };
